877146255495f79d7c557ab20ac674ec0e9fc841
sarthakbob <70557482+sarthakbob@users.noreply.github.com>
2020-11-25 19:09:51 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+function preload(){
+  car1_image = loadImage("images/car1.png");
+  car2_image = loadImage("images/car2.png");
+  car3_image = loadImage("images/car3.png");
+  car4_image = loadImage("images/car4.png");
+  track_image = loadImage("images/track.jpg");
+  ground_image = loadImage("images/ground.png");
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState === 2){
+    game.end();
+  }
+}
